0b7c0a422c0cc8fdc8a7a2453c00d590c94a46f5
Betül Iskender <betul.iskender@outlook.dk>
2023-01-17 07:46:15 +0100

    lille ændring

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <h3>Home</h3>
-      
+        <h3 className='center'>Welcome to FullStack Time Tracker System</h3>
     </div>
   )
 }
